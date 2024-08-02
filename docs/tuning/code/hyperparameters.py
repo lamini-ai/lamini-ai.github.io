4cb137ffcb26a9f725a63245191b6f9679787fc2
Eda Z <36808054+edamamez@users.noreply.github.com>
2024-08-02 14:00:01 -0700

    Update to 3.0.0a1 (#128)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- docs/tuning/code/hyperparameters.py
+++ docs/tuning/code/hyperparameters.py
@@ -1,6 +1,6 @@
 from lamini import Lamini
 
-llm = Lamini(model_name='meta-llama/Meta-Llama-3.1-8B-Instruct')
+llm = Lamini(model_name='meta-llama/Meta-Llama-3-8B-Instruct')
 data = [{
     "input": "What is Lamini? Is it like a robot or a computer program?",
     "output": "Lamini is a program for the execution of LLMs called a large language model engine. It is not a robot, but rather a tool for building and executing LLMs.",
